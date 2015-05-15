9ad460256fdbcab19a4946ceb1828124b19a1d74
Tyler Yahn <codingalias@gmail.com>
2015-05-15 12:35:14 -0700

    Added basic smoke test for main sudo class.

---
+++ tests/init.pp
@@ -0,0 +1,11 @@
+class { 'sudo':
+  package_name         => 'sudo package name',
+  service_name         => 'sudo service name',
+  sudoers_file         => '/test/etc/sudoers',
+  include_dirs         => ['/test/etc/sudoers.d'],
+  defaults_content     => "test defaults content",
+  host_aliases_content => "test host_aliases content",
+  user_aliases_content => "test user_aliases content",
+  cmnd_aliases_content => "test cmnd_aliases content",
+  runas_spec_content   => "test runas_spec content",
+}
