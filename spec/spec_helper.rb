8f30dd02eb086c79d9bb0e75c158674ac49ed887
Tyler Yahn <tyler@moonshadowmobile.com>
2015-09-14 13:39:08 -0700

    Update spec_helper.
    Add generic location for the concat to store files for all tests.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,11 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
 
+fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
+
+RSpec.configure do |c|
+  c.default_facts = {
+    :concat_basedir => File.join(fixture_path, 'concat_basedir')
+  }
+end
+
 at_exit { RSpec::Puppet::Coverage.report! }
